6b639b09a378e83b0f22c92f8ef2b11eefbb04f4
M Pradyumna <160750412+pradyu25@users.noreply.github.com>
2025-11-22 20:57:54 +0530

    Register models in Django admin

--- ResumeAnalysisApp/admin.py
+++ ResumeAnalysisApp/admin.py
@@ -1,1 +1,9 @@
+# ResumeAnalysisApp/admin.py
+from django.contrib import admin
+from .models import Signup, Feedback, JobPost, ResumeUpload
+
+admin.site.register(Signup)
+admin.site.register(Feedback)
+admin.site.register(JobPost)
+admin.site.register(ResumeUpload)
 
